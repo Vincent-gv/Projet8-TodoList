f8aaba3dc30c4c536b0082b47a54a03151cb6437
vincent <vinzmass@gmail.com>
2020-12-16 07:17:22 +0100

    Add User Roles: Admin and User
    Update access for user or admin + add conditions in templates + Add Access Denied Handler to display a message when user is not allowed to see a page

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    private UrlGeneratorInterface $urlGenerator;
+    private SessionInterface $session;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator, SessionInterface $session)
+    {
+        $this->urlGenerator = $urlGenerator;
+        $this->session = $session;
+    }
+
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        // add a custom flash message and redirect to the login page
+        $this->session->getFlashBag()->add('error', 'Seuls les utilisateurs ayant le rôle administrateur peuvent accéder aux pages de gestion des utilisateurs.');
+
+        return new RedirectResponse($this->urlGenerator->generate('homepage'));
+
+    }
+}
